eddb0a684333a35d1daead41e9868b10cb0cca6d
MishaliaPillay <2435450@students.wits.ac.za>
2024-05-15 10:29:16 +0200

    images and json data

--- src/Pages/Art/Art.jsx
+++ src/Pages/Art/Art.jsx
@@ -1,12 +1,142 @@
-import React from 'react'
-
+import React from "react";
+import "./Art.css";
 const Art = () => {
   return (
     <div>
-      
-edddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+      <p className="Test">
+        edddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit. Est ipsum, deserunt at alias culpa
+        doloribus ea perspiciatis repellat, aspernatur eligendi libero velit,
+        non cupiditate fugit amet ab animi sint corporis.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Art
+export default Art;
