fae1d6d0924042b16a283c290b1bea30781adc40
MishaliaPillay <2435450@students.wits.ac.za>
2024-05-23 20:19:47 +0200

    Update OceanFacts.jsx

--- src/Pages/Home/OceanFacts.jsx
+++ src/Pages/Home/OceanFacts.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import oceanFactsData from "../../Data/101Facts.json";
 
-console.log(oceanFactsData); // Check if the data is imported correctly
-
 const Fact = ({ id, fact }) => (
-  <li key={id} className="fact">{fact}</li>
+  <li key={id} className="fact">
+    {fact}
+  </li>
 );
 
 const OceanFacts = () => {
   const { ocean_facts } = oceanFactsData;
-  const allFacts = ocean_facts || [];
+  const [displayedFacts, setDisplayedFacts] = useState([]);
+
+  const refreshFacts = () => {
+    // Shuffle the array of facts
+    const shuffledFacts = ocean_facts.sort(() => Math.random() - 0.5);
+    // Take the first 10 shuffled facts
+    const selectedFacts = shuffledFacts.slice(0, 10);
+    setDisplayedFacts(selectedFacts);
+  };
+
+  // Initial loading of 10 random facts
+  useState(refreshFacts);
 
   return (
     <div className="ocean-facts">
       <h1>Ocean Facts</h1>
       <ul>
-        {allFacts.map(({ id, fact }) => (
+        {displayedFacts.map(({ id, fact }) => (
           <Fact key={id} id={id} fact={fact} />
         ))}
       </ul>
+      <button onClick={refreshFacts} style={{ cursor: "pointer" }}>
+        More Facts
+      </button>
     </div>
   );
 };
