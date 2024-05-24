dbb84bff8b6ac502b0833cad52dd6c1c165424e1
MishaliaPillay <2435450@students.wits.ac.za>
2024-05-24 19:35:15 +0200

    theory

--- src/Pages/Theory/Theory.jsx
+++ src/Pages/Theory/Theory.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import React from "react";
 
 const Theory = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <section>
+        <h1>Critical Analysis Essay on Net/Web Art</h1> <h2>Introduction</h2>{" "}
+        <p>
+          This essay is a critical examination of the Net Art, “it’s doing It”.
+          This is an artwork that was created by nine artists who wrote computer
+          programs that took instructions as parameters, and this resulted in
+          computer-generated art. This analysis will be carried out through the
+          usage of the framework by Marshall McLuhan, “The medium is the
+          message”, with a focus on the effects of choosing the Internet as the
+          medium for this work. An evaluation through the lens of Intermedial
+          studies will serve as a framework to unpack the content of the work
+          itself. The analysis will help determine if “It’s doing It”, is a
+          relevant and effective example of Net Art that can inspire and inform
+          the designers of today.{" "}
+        </p>{" "}
+        <h2>What is Net Art?</h2>
+        <p>
+          For one to understand Net Art one needs to break the bounds of what
+          media forms art can be created in (Ricci, 2020). Conventionally art is
+          in the form of a painting or a sculpture, Benedetta Ricci challenges
+          readers to “envision an algorithm, software or code” as art. Net Art
+          is an art that cannot exist without the Internet, it is created for,
+          composed through, and displayed on the Internet.
+        </p>
+        <p>
+          Chatel (2019) describes Net Art as “process-oriented” as artists
+          explore the vastness of this new media type, thus the spectrum of
+          topics and areas covered in Net Art is endless. Artists tackle the
+          complex nature of “networks and computing structures”- this is
+          important to note since designers create these structures with code,
+          and it is through the creation of Net Art that they become “artists”
+          who challenge these very systems.
+        </p>
+        <p>
+          A “sub-genre” of Net Art is Software Art which is “visualizing the
+          result from a set of algorithms and codes”. Based on the data given to
+          the algorithm different results may be yielded, the visual aspect
+          helps identify gaps in the data which is a useful tool that can aid in
+          the prevention of misrepresentation. Software art is created when the
+          concept is encoded in the system by the artists and beyond the code
+          the artist has no control over the resulting artwork, thus unique and
+          interesting visuals arise.
+        </p>
+        <h2>Explain “it’s doing It” and its relevance for designers.</h2>
+        <p>
+          This artwork falls part of Software art, there are a total of nine
+          artists that worked on this project and each artist created a computer
+          program that encoded instructions (see Figures 1-9) into the program
+          and for 45 days these programs generated one image each day. This Net
+          Art ran from 11 December 2015 till 24 January 2016, and every artist's
+          computer program generated an image related to their instruction – the
+          viewer is unable to see the code and only sees the resulting images.
+          This was an interesting project since the computer programs ran the
+          same code every day, yet the results differed in small and large ways
+          each time. This is equivalent to a user asking ChatGPT the same
+          question every day in the exact same format as they did the previous
+          day, but the AI responds uniquely each time.{" "}
+        </p>{" "}
+        <figure>
+        
+          <img src="../../Images/Essay/prompt1.png" alt="" />
+          <figcaption>Figure 1 Prompt 1</figcaption>
+        </figure>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </section>
+    </>
+  );
+};
 
-export default Theory
+export default Theory;
