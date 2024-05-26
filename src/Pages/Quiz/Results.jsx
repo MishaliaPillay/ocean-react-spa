c0c30669a0c68db5552bd32a61c641b559ce47b6
MishaliaPillay <2435450@students.wits.ac.za>
2024-05-26 21:06:16 +0200

    last bits

--- src/Pages/Quiz/Results.jsx
+++ src/Pages/Quiz/Results.jsx
@@ -2,28 +2,35 @@ import React from "react";
 
 // Component to render quiz results
 const Results = ({
-  quizData,
-  sectionIndex,
-  userAnswers,
-  handleBackToSections,
+  quizData, // The quiz data containing all the Q&A
+  sectionIndex, // The index of the current quiz section
+  userAnswers, // Array of answers selected by  user
+  handleBackToSections, // Function to navigate back to the section selection view
 }) => {
+  // Extract the current section questions using the section index
   const currentSection =
     quizData.questions[0][Object.keys(quizData.questions[0])[sectionIndex]];
 
   return (
     <section className="results-container">
       <h2>Quiz Results</h2>
+
+      {/* Iterate over user answers to display each question result */}
       {userAnswers.map((answer, index) => (
         <section key={index}>
           <p className="heading">
             Question {index + 1}:{" "}
             {answer === currentSection[index].answer ? "Correct" : "Incorrect"}
           </p>
+
+          {/* If the user's answer was incorrect, display the correct answer explanation */}
           {answer !== currentSection[index].answer && (
             <p>{currentSection[index].explanation}</p>
           )}
         </section>
       ))}
+
+      {/* Button to navigate back to the section selection */}
       <button className="back-button" onClick={handleBackToSections}>
         Back to Sections
       </button>
--- src/Pages/Quiz/SectionCards.jsx
+++ src/Pages/Quiz/SectionCards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // Component to render section cards for quiz selection
-const SectionCards = ({ quizData, startQuiz }) => {
+const SectionCards = ({ quizData, startQuiz }) => {  // Each key represents a section of the quiz
   return Object.keys(quizData.questions[0]).map((sectionKey, index) => (
     <section className="section-card" key={index}>
       <h2>{sectionKey.charAt(0).toUpperCase() + sectionKey.slice(1)}</h2>
